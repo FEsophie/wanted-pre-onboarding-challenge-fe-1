c544ffff36a44b37bdce0a1b1e9492c9e452d817
FEsophie <sophie220207@gmail.com>
2022-11-14 01:48:54 +0900

    코드수정

--- front/src/utils/index.js
+++ front/src/utils/index.js
@@ -1,24 +1,24 @@
 class Util {
-    constructor() {}
+  constructor() {}
 
-    uuidV4(short) {
-        const cryptoObj = window.crypto;
-        const id = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-            (
-                c ^
-                (cryptoObj.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-            ).toString(16)
-        );
+  uuidV4(short) {
+    const cryptoObj = window.crypto;
+    const id = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (cryptoObj.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
 
-        return short ? id.substring(0, 8) : id;
-    }
+    return short ? id.substring(0, 8) : id;
+  }
 
-    isValidEmail(email) {
-        const emailRegex =
-            /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  isValidEmail(email) {
+    const emailRegex =
+      /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
-        return emailRegex.test(email);
-    }
+    return emailRegex.test(email);
+  }
 }
 
 export default new Util();
