2b3d0371f8cacbf19be573c66a145810c86d90e6
FEsophie <sophieSophie@trenbeui-MacBookPro-3.local>
2022-11-13 20:00:32 +0900

    디렉토리 수정

---
+++ front/src/service/todos/keys.ts
@@ -0,0 +1,6 @@
+const base = "todos";
+
+export const GET_TODO_LIST = `${base}/GET_TODO_LIST`;
+export const ADD_TODO_ITEM = `${base}/ADD_TODO_ITEM`;
+export const UPDATE_TODO_ITEM = `${base}/UPDATE_TODO_ITEM`;
+export const DELETE_TODO_ITEM = `${base}/DELETE_TODO_ITEM`;
---
+++ front/src/service/users/types.ts
@@ -0,0 +1,9 @@
+export interface LoginReq {
+    email: string;
+    password: string;
+}
+
+export interface LoginRes {
+    message: string;
+    token: string;
+}
