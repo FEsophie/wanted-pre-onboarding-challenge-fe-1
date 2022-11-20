adc548adec1b23bb48d2b5a34097443e4e62cbae
FEsophie <sophie220207@gmail.com>
2022-11-20 17:20:53 +0900

    충돌 해결

--- front/src/containers/AuthPage/login.tsx
+++ front/src/containers/AuthPage/login.tsx
@@ -1,7 +1,4 @@
-import React, {
-    useRef,
-    useState,
-} from "react";
+import React, { useRef, useState } from "react";
 import TextInput from "../../components/TextInput";
 import styled from "styled-components";
 import Button from "../../components/Button";
@@ -9,126 +6,117 @@ import ButtonGroup from "../../components/ButtonGroup";
 import { useUserLogin } from "../../service/users/useUserService";
 import { FormGroup } from "./index";
 import { useNavigate } from "react-router-dom";
-import {
-    atom,
-    selector,
-    useRecoilState,
-    useRecoilValue,
-    useSetRecoilState,
-} from "recoil";
+import { useSetRecoilState } from "recoil";
 import { loginUser } from "../../module/atom";
 import { useToast } from "../../hooks/useToast";
 
 interface input {
-    value: string;
-    error: boolean;
+  value: string;
+  error: boolean;
 }
 
 type refType = Partial<{ emailInput: input; passwordInput: input }>;
 
 function Login() {
-    const setLoginUser = useSetRecoilState(loginUser);
+  const setLoginUser = useSetRecoilState(loginUser);
+  const navigator = useNavigate();
+  const inputGroupRef = useRef<refType>({});
+  const [isActive, setActive] = useState(false);
+  const fetchLoginMutation = useUserLogin();
+  const toast = useToast();
 
-    const navigator = useNavigate();
-    const inputGroupRef = useRef<refType>({});
-    const [isActive, setActive] = useState(false);
+  function onSetUserEmail(value: string) {
+    setLoginUser({ userEmail: value });
+  }
 
-    function onSetUserEmail(value: string) {
-        setLoginUser({ userEmail: value });
+  function setInputRef(type: "emailInput" | "passwordInput") {
+    return (ref: HTMLInputElement | null, isValid?: boolean) => {
+      const value = ref?.value;
+      inputGroupRef.current = {
+        ...inputGroupRef?.current,
+        ...{
+          [type]: {
+            value,
+            error: isValid,
+          },
+        },
+      };
+      onSetActive();
+    };
+  }
+
+  function onSetActive() {
+    if (
+      !inputGroupRef?.current?.emailInput?.error &&
+      !inputGroupRef?.current?.passwordInput?.error
+    ) {
+      setActive(true);
+    } else {
+      setActive(false);
     }
+  }
 
-    function setInputRef(type: "emailInput" | "passwordInput") {
-        return (ref: HTMLInputElement | null, isValid?: boolean) => {
-            const value = ref?.value;
-            inputGroupRef.current = {
-                ...inputGroupRef?.current,
-                ...{
-                    [type]: {
-                        value,
-                        error: isValid,
-                    },
-                },
-            };
-            onSetActive();
-        };
-    }
+  function onSubmit() {
+    const { emailInput, passwordInput } = inputGroupRef.current;
 
-    function onSetActive() {
-        if (
-            !inputGroupRef?.current?.emailInput?.error &&
-            !inputGroupRef?.current?.passwordInput?.error
-        ) {
-            setActive(true);
-        } else {
-            setActive(false);
-        }
-    }
+    const email = emailInput?.value || "";
+    const password = passwordInput?.value || "";
 
-    const fetchLoginMutation = useUserLogin();
-
-    function onSubmit() {
-        const { emailInput, passwordInput } = inputGroupRef.current;
-
-        const email = emailInput?.value || "";
-        const password = passwordInput?.value || "";
-
-        const toast = useToast();
-
-        fetchLoginMutation.mutate(
-            { email, password },
-            {
-                onSuccess(res) {
-                    localStorage.setItem("authToken", res.token);
-                    onSetUserEmail(email);
-                    toast("로그인 성공", { type: "success" });
-                },
-                onError() {
-                    toast("로그인 실패", { type: "error" });
-                },
-            }
-        );
-    }
-
-    return (
-        <LoginSection>
-            <FormGroup>
-                <TextInput
-                    id={"id-input"}
-                    text="아이디(이메일)"
-                    placeholder={"이메일을 입력해주세요"}
-                    onRef={setInputRef("emailInput")}
-                    errorMessage={"@,.이 포함된 이메일 유형을 입력해주세요."}
-                    type="email"
-                    onChange={setInputRef("emailInput")}
-                />
-                <TextInput
-                    id={"pwd-input"}
-                    onRef={setInputRef("passwordInput")}
-                    type="password"
-                    text="비밀번호"
-                    placeholder={"비밀번호를 입력해주세요"}
-                    emptyMessage={"비밀번호를 입력해주세요"}
-                    minLength={8}
-                    onChange={setInputRef("passwordInput")}
-                />
-                <ButtonGroup align="right">
-                    <Button
-                        disabled={!isActive}
-                        styleType={"secondary"}
-                        onClick={onSubmit}
-                    >
-                        로그인
-                    </Button>
-                    <Button styleType={"primary"} onClick={() => navigator("/signup")}>
-                        회원 가입
-                    </Button>
-                </ButtonGroup>
-                <MoveToHomeBtn styleType="light" onClick={() => navigator("/")}>
-                    홈으로 이동
-                </MoveToHomeBtn>
-            </FormGroup>
-        </LoginSection>
+    fetchLoginMutation.mutate(
+      { email, password },
+      {
+        onSuccess() {
+          onSetUserEmail(email);
+          toast("로그인 성공", { type: "success" });
+          navigator("/");
+        },
+        onError() {
+          toast("로그인 실패", { type: "error" });
+        },
+      }
     );
+  }
+
+  return (
+    <LoginSection>
+      <FormGroup>
+        <TextInput
+          id={"id-input"}
+          text="아이디(이메일)"
+          placeholder={"이메일을 입력해주세요"}
+          onRef={setInputRef("emailInput")}
+          errorMessage={"@,.이 포함된 이메일 유형을 입력해주세요."}
+          type="email"
+          onChange={setInputRef("emailInput")}
+        />
+        <TextInput
+          id={"pwd-input"}
+          onRef={setInputRef("passwordInput")}
+          type="password"
+          text="비밀번호"
+          placeholder={"비밀번호를 입력해주세요"}
+          emptyMessage={"비밀번호를 입력해주세요"}
+          minLength={8}
+          onChange={setInputRef("passwordInput")}
+        />
+        <ButtonGroup align="right">
+          <Button
+            disabled={!isActive}
+            styleType={"secondary"}
+            onClick={onSubmit}
+          >
+            로그인
+          </Button>
+          <Button styleType={"primary"} onClick={() => navigator("/signup")}>
+            회원 가입
+          </Button>
+        </ButtonGroup>
+        <MoveToHomeBtn styleType="light" onClick={() => navigator("/")}>
+          홈으로 이동
+        </MoveToHomeBtn>
+      </FormGroup>
+    </LoginSection>
+  );
 }
 
 const LoginSection = styled.section`
--- front/src/containers/SignUpPage/index.tsx
+++ front/src/containers/SignUpPage/index.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import Signup from "./signup";
 import { UserPageContainer } from "../AuthPage";
+import { useLocation } from "react-router-dom";
+import Button from "../../components/Button";
 
 function SignUpPage() {
-    return (
-        <UserPageContainer>
-            <div>
-                <h2>회원 가입</h2>
-                <Signup />
-            </div>
-        </UserPageContainer>
-    );
+  const location = useLocation();
+
+  const isCompleteSignUp = useMemo(
+    () => location.search.includes("step=complete"),
+    [location.search]
+  );
+  return (
+    <UserPageContainer>
+      {isCompleteSignUp ? (
+        <div>
+          <p>회원 가입 완료</p>
+          <Button type={"link"} url={"/auth"} styleType={"primary"}>
+            로그인 하러가기
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <h2>회원 가입</h2>
+          <Signup />
+        </div>
+      )}
+    </UserPageContainer>
+  );
 }
 
 export default SignUpPage;
